ba115975cd73e1140700d8260092bec3306bf841
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-07-22 23:11:54 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware-1");
-  next();
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  //   console.log("in the middleware add-product");
+  res.send(
+    '<form action="/products" method="POST"><input type="text" name="title"/> <input type="number" name="size"/> <button type="submit">Submit</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-    console.log("in the middleware-2");
-    res.send('<h1>Hello from Express JS!.</h1>')
-  });
+app.use("/products", (req, res, next) => {
+  console.log(req.body);
+  // res.send("<h1>Hello from Express JS path!.</h1>");
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  //   console.log("In the middleware");
+  res.send("<h1>Hello from Express JS path!.</h1>");
+});
 
 app.listen(4000);
